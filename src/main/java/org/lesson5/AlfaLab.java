86e56e6920d584d0eff5c1d819fc4b11f6d2fc14
Vladimir Efremov <vefremov@alfabank.ru>
2026-05-08 12:44:50 +0400

    Выполнение пятого задания

--- src/main/java/org/lesson5/AlfaLab.java
+++ src/main/java/org/lesson5/AlfaLab.java
@@ -1,16 +1,54 @@
 package org.lesson5;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 public class AlfaLab {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // Задаем необходимые переменные
+        int minLength = 8; // минимальная длина
+        int[] seed = {42, 77, 13};
+        String prefix = "AGENT";
 
-    public String generateAccesToken() {
-        String accessToken;
-        accessToken = toString(LocalDate.now().getYear());
-        return accessToken;
+        // Выполняем задание
+        String accessCode = generateAccessCode();
+        System.out.println("Результат проверки: " + isValidCode(accessCode, minLength));
+        logEvent("Server protection activated");
+        logEvent("Intrusion attempt detected", true);
+        for (int j : seed) {
+            System.out.println(generateAgentId(prefix, j));
+        }
     }
 
-    private String toString(int year) {
+// Протокол ГЕНЕРАТОР
+    public static String generateAccessCode() {
+        String year = Integer.toString(LocalDate.now().getYear());
+        int pow = (int)Math.pow(3,7);
+        return year.concat("-").concat(Integer.toString(pow));
+    }
+
+// Протокол ВАЛИДАТОР
+    private static boolean isValidCode(String code, int minLength) {
+        return code != null && code.length() >= minLength && code.contains("-");
+    }
+
+// Протокол ЛОГГЕР
+    public static void logEvent(String message) {
+        System.out.println("[INFO] " + message);
+    }
+
+    public static void logEvent(String message, boolean isCritical) {
+        if (isCritical) {
+            System.out.println("[CRITICAL] " + message);
+        } else {
+            logEvent(message);
+        }
+    }
+
+// Протокол РАНДОМАЙЗЕР
+    public static String generateAgentId(String prefix, int seed) {
+        Random random = new Random(seed);
+        int randomInt = Math.abs(random.nextInt(1000, 10000));
+        return prefix.concat("-").concat(String.valueOf(randomInt));
     }
 }
