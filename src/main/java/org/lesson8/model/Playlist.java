b402dc453910a11e591583793e52e3923e03ac1e
Vladimir Efremov <vefremov@alfabank.ru>
2026-05-29 15:09:33 +0400

    Исправление по итогам ревью 2

--- src/main/java/org/lesson8/model/Playlist.java
+++ src/main/java/org/lesson8/model/Playlist.java
@@ -11,29 +11,21 @@ public class Playlist {
         this.songsList = songsList;
     }
 
-    public Playlist() {}
-
-    //  Добавление песни в плейлист
-    public void addSong (String songName) {
+    public void addSong(String songName) {
         songsList.add(songName);
         System.out.println("В плейлист добавлена песня: '" + songName + "'");
     }
 
-    //  Удаление песни из плейлиста
-    public void removeSong (int index) {
-        String songForDeletion = songsList.get(index);
-        songsList.remove(index);
+    public void removeSong(int index) {
+        String songForDeletion = songsList.remove(index);
         System.out.println("Из плейлиста удалена песня: '" + songForDeletion + "'");
     }
 
-    //  Обновление песни в плейлисте по индексу
-    public void updateSong (int index, String songName) {
-        String oldSong = songsList.get(index);
-        songsList.set(index, songName);
+    public void updateSong(int index, String songName) {
+        String oldSong = songsList.set(index, songName);
         System.out.println("Плейлист обновлен: песня '" + oldSong + "' заменена на '" + songName + "'");
     }
 
-//    Получение песни по индексу
     public String getSongName(int index) {
         String song = songsList.get(index);
         System.out.println("Из плейлиста получена песня: '" + song + "'");
