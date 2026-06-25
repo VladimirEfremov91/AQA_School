26f63fc564c9213bf1644e79de6698beeb1e418b
Vladimir Efremov <vefremov@alfabank.ru>
2026-06-25 16:10:04 +0400

    10 Задание - Трекер задач - Создание класса Task

--- src/main/java/org/lesson10/Task.java
+++ src/main/java/org/lesson10/Task.java
@@ -1,4 +1,27 @@
 package org.lesson10;
 
 public class Task {
+    private String taskName;
+    private Boolean  taskStatus;
+
+    public Task(String taskName, Boolean taskStatus) {
+        this.taskName = taskName;
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Boolean getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(Boolean taskStatus) {
+        this.taskStatus = taskStatus;
+    }
 }
