58e0d9ff3385e46eda8127bef89cdfeccb590e62
Vladimir Efremov <vefremov@alfabank.ru>
2026-06-26 17:40:16 +0400

    10 Задание - Трекер задач - Исправления и оптимизации кода классов Task и TaskTracker

--- src/main/java/org/lesson10/model/TaskTracker.java
+++ src/main/java/org/lesson10/model/TaskTracker.java
@@ -3,14 +3,22 @@ package org.lesson10.model;
 import java.util.ArrayList;
 
 public class TaskTracker {
-    ArrayList<Task> tasks = new ArrayList<Task>();
-    Task task = new Task();
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+//    Универсальный метод поиска задач
+private Task findTask(String taskName) {
+    for (Task task : tasks) {
+        if (task.getTaskName().equals(taskName)) {
+            return task;
+        }
+    }
+    return null;
+}
 
 //    Метод создания новой задачи (по умолчанию создается невыполненная задача)
     public void addNewTask(String taskName) {
-        task.setTaskName(taskName);
-        task.setIsTaskCompleted(false);
-        tasks.addLast(task);
+        Task task = new Task(taskName);
+        tasks.add(task);
     }
 
 //    Смотрим все задачи
@@ -22,13 +30,7 @@ public class TaskTracker {
 
 //    Отмечаем задачу выполненной
     public void setTaskCompleted(String taskName) {
-        Task foundTask = null;
-        for (Task task : tasks) {
-            if (task.getTaskName().equals(taskName)) {
-                foundTask = task;
-                break;
-            }
-        }
+        Task foundTask = findTask(taskName);
         if (foundTask != null) {
             foundTask.setIsTaskCompleted(true);
             System.out.println("Задача \"" + taskName + "\" отмечена как выполненная");
@@ -39,15 +41,9 @@ public class TaskTracker {
 
 //    Получаем статус задачи по имени
     public void getTaskStatus(String taskName) {
-        Task foundTask = null;
-        for (Task task : tasks) {
-            if (task.getTaskName().equals(taskName)) {
-                foundTask = task;
-                break;
-            }
-        }
+        Task foundTask = findTask(taskName);
         if (foundTask != null) {
-            if (foundTask.getIsTaskCompleted() == true) {
+            if (foundTask.getIsTaskCompleted()) {
                 System.out.println("Статус задачи \"" + taskName + "\": ВЫПОЛНЕНО");
             } else {
                 System.out.println("Статус задачи \"" + taskName + "\": НЕ ВЫПОЛНЕНО");
@@ -57,24 +53,19 @@ public class TaskTracker {
         }
     }
 
-//    Получение статистики по задачам
+//    Получение статистики по всем задачам
     public void getTaskStatistics() {
         int completedTasks = 0;
-        int openTasks = 0;
         int totalTasks = tasks.size();
         for (Task task : tasks) {
-            if (task.getIsTaskCompleted() == true) {
+            if (task.getIsTaskCompleted()) {
                 completedTasks++;
-            } else {
-                openTasks++;
             }
         }
+        int openTasks = totalTasks - completedTasks;
         System.out.println("Статистика по задачам:");
         System.out.println("Всего задач: " + totalTasks);
         System.out.println("Выполнено: " + completedTasks);
         System.out.println("Открыто: " + openTasks);
-
     }
-
-
 }
