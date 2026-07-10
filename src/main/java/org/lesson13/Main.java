3fd447c49bcaaa4b1b9e4e886603b46325e370f5
Vladimir Efremov <vefremov@alfabank.ru>
2026-07-10 12:14:23 +0400

    13 Задание - Выполнение 1 части: База данных пришельцев

---
+++ src/main/java/org/lesson13/Main.java
@@ -0,0 +1,29 @@
+package org.lesson13;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <Alien> aliens = new ArrayList<>();
+        aliens.addLast(new Alien("Дарт", "Татуин", 8));
+        aliens.addLast(new Alien("Фредди", "Вязовая", 10));
+        aliens.addLast(new Alien("Альф", "Альфа-Центавра", 1));
+        aliens.addLast(new Alien("Глазастик", "Регулус", 3));
+        aliens.addLast(new Alien("Глазастик", "Регулус", 6));
+
+        //Ищем дубликаты пришельцев
+        boolean hasDuplicate = false;
+        for (int i = 0; i < aliens.size(); i++) {
+            for (int j = i + 1; j < aliens.size(); j++) {
+                if (aliens.get(i).equals(aliens.get(j))) {
+                    hasDuplicate = true;
+                    break;
+                }
+            }
+            if (hasDuplicate) {
+                break;
+            }
+        }
+        System.out.println("Список содержит дубликат: " + hasDuplicate);
+    }
+}
