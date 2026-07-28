056244f27f6236e546b063d39a01cc86243471cc
Vladimir Efremov <vefremov@alfabank.ru>
2026-07-28 09:38:59 +0400

    15 Задание - Тесты метода возврата игры

--- src/test/java/org/lesson15/BoardGameRentalTest.java
+++ src/test/java/org/lesson15/BoardGameRentalTest.java
@@ -10,7 +10,7 @@ public class BoardGameRentalTest {
     void testRentBoardGameHappyPath() {
         BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
         boardGameCatalog.addGame(new BoardGame("Зомби 3", 18, 33.1));
-        assertTrue(boardGameCatalog.rentBoardGame("Зомби 3", 21),
+        assertTrue(boardGameCatalog.rentGame("Зомби 3", 21),
                 "Метод аренды вернул неожиданный ответ");
         assertTrue(boardGameCatalog.findBoardGameByTitle("Зомби 3").isRent(),
                 "Метод аренды не проставил признак того, что игра в аренде");
@@ -21,7 +21,7 @@ public class BoardGameRentalTest {
         BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
         boardGameCatalog.addGame(new BoardGame("Зомби 3", 18, 33.1));
         assertThrows(IllegalArgumentException.class,
-                () -> boardGameCatalog.rentBoardGame("Зомби 4", 21),
+                () -> boardGameCatalog.rentGame("Зомби 4", 21),
                 "Метод IllegalArgumentException при несуществующем названии");
     }
 
@@ -29,15 +29,46 @@ public class BoardGameRentalTest {
     void testRentBoardGameInvalidClientAge() {
         BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
         boardGameCatalog.addGame(new BoardGame("Зомби 3", 18, 33.1));
-        assertFalse(boardGameCatalog.rentBoardGame("Зомби 3", 3),
+        assertFalse(boardGameCatalog.rentGame("Зомби 3", 3),
                 "Метод аренды вернул неожиданный ответ");
     }
 
+    @Test
     void testRentBoardGameAlreadyRented() {
         BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
         BoardGame boardGame = new BoardGame("Зомби 3", 18, 33.1);
         boardGame.setRent(true);
-        assertFalse(boardGameCatalog.rentBoardGame("Зомби 3", 21),
+        boardGameCatalog.addGame(boardGame);
+        assertFalse(boardGameCatalog.rentGame("Зомби 3", 21),
+                "Метод аренды вернул неожиданный ответ");
+    }
+
+    @Test
+    void testReturnBoardGameHappyPath() {
+        BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
+        BoardGame boardGame = new BoardGame("Зомби 3", 18, 33.1);
+        boardGame.setRent(true);
+        boardGameCatalog.addGame(boardGame);
+        assertTrue(boardGameCatalog.rentGame("Зомби 3", 21),
+                "Метод returnBoardGame должен вернуть true при успешном возврате арендованной игры");
+        assertFalse(boardGame.isRent(),
+                "После успешного возврата игра не должна иметь признак аренды"
+        );
+    }
+
+    @Test
+    void testReturnBoardGameNotFound() {
+        BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
+        boardGameCatalog.addGame(new BoardGame("Зомби 3", 18, 33.1));
+        assertFalse(boardGameCatalog.returnBoardGame("Зомби 4"),
+                "Метод returnBoardGame должен вернуть false при возврате несуществующей игры");
+    }
+
+    @Test
+    void testReturnBoardGameNotRented() {
+        BoardGameCatalog boardGameCatalog = new BoardGameCatalog();
+        boardGameCatalog.addGame(new BoardGame("Зомби 3", 18, 33.1));
+        assertTrue(boardGameCatalog.returnBoardGame("Зомби 3"),
                 "Метод аренды вернул неожиданный ответ");
     }
 
