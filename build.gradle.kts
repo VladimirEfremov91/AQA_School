2b0c89e5bcf301a993e931ac432829f942c0180b
Vladimir Efremov <vefremov@alfabank.ru>
2026-08-03 15:55:03 +0400

    16 Задание - Выполнение задания

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,15 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-suite")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation(platform("io.cucumber:cucumber-bom:7.34.4"))
+    testImplementation("io.cucumber:cucumber-java")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine")
+
     implementation("net.datafaker:datafaker:2.5.4")
     implementation("com.github.lalyos:jfiglet:0.0.8")
-
 }
 
 tasks.test {
---
+++ src/test/java/org/lesson16/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package org.lesson16;
+
+import io.cucumber.core.options.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@smoke")
+
+public class RunCucumberTest {
+}
