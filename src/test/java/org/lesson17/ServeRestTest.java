a685938377a8f0f04d4b5f3c74ac28611937de3d
Vladimir Efremov <vefremov@alfabank.ru>
2026-08-10 11:20:12 +0400

    17 Задание - Выполнение 3 части задания

--- src/test/java/org/lesson17/ServeRestTest.java
+++ src/test/java/org/lesson17/ServeRestTest.java
@@ -2,6 +2,7 @@ package org.lesson17;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,8 @@ import static org.hamcrest.Matchers.*;
 
 
 public class ServeRestTest {
+
+//  Задание 1. Открываем магазин — настройка
     @BeforeAll
     static void setup() {
         RestAssured.baseURI = "https://serverest.dev";
@@ -26,5 +29,23 @@ public class ServeRestTest {
                 .contentType(ContentType.JSON)
                 .body("quantidade", greaterThan(0))
                 .body("usuarios.size()", greaterThan(0));
+    }
+
+    //  Задание 3. «Досье на клиента» — GET с query-параметром
+    @Test
+    public void shouldFindUserByEmail() {
+
+        String email = when()
+                .get("/usuarios")
+                .then()
+                .extract()
+                .path("usuarios[0].email");
+
+        given().queryParam("email", email)
+                .when().get("/usuarios")
+                .then().log().all()
+                .statusCode(200)
+                .body("quantidade", equalTo(1))
+                .body("usuarios[0].email", equalTo(email));
     }
 }
